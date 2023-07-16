d4679995773ba7a3c980a28e59810275683480f2
Oksidian Tafly <oksitafly@gmail.com>
2023-07-16 19:53:09 +0800

    + Add and connect mongodb

--- go-app/app.go
+++ go-app/app.go
@@ -1,32 +1,48 @@
 package main
 
 import (
+	"go-user-app/app/configs"
+
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
+var userCollection *mongo.Collection = configs.GetCollection(configs.DB, "users")
+
 type User struct {
-	ID          string `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"`
+	ID          primitive.ObjectID `json:"id,omitempty"`
+	Name        string             `json:"name"`
+	Description string             `json:"description"`
 }
 
-func GetUserByID(id string) (User, error) {
-	// TODO : lookup in db
-	return User{
-		ID:          id,
-		Name:        "Doe",
-		Description: "John",
-	}, nil
+func GetUserByID(c *gin.Context, id string) (User, error) {
+	var user User
+	objId, _ := primitive.ObjectIDFromHex(id)
+	err := userCollection.FindOne(c, bson.M{"id": objId}).Decode(&user)
+	return user, err
+}
+
+func CreateUser(c *gin.Context, name string, description string) (User, error) {
+	user := User{
+		ID:          primitive.NewObjectID(),
+		Name:        name,
+		Description: description,
+	}
+
+	_, err := userCollection.InsertOne(c, user)
+	return user, err
 }
 
 func GetUserHandler(c *gin.Context) {
 	// retrieve user from db
 	id := c.Query("id")
-	user, err := GetUserByID(id)
+	user, err := GetUserByID(c, id)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "impossible to retrieve user"})
 		return
@@ -38,7 +54,25 @@ func GetDefaultHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, "Hello World!")
 }
 
+func PostUserHandler(c *gin.Context) {
+	// parse user from json
+	var user User
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Bad request"})
+		return
+	}
+	parsedUser, err := CreateUser(c, user.Name, user.Description)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "impossible to add user"})
+		return
+	}
+	c.JSON(http.StatusOK, parsedUser)
+}
+
 func main() {
+	//run database
+	configs.ConnectDB()
+
 	r := gin.Default()
 	r.GET("/", GetDefaultHandler)
 	g := r.Group("/apis/goapp")
@@ -46,6 +80,7 @@ func main() {
 	g.GET("", GetDefaultHandler)
 	g.GET("/", GetDefaultHandler)
 	g.GET("/user", GetUserHandler)
+	g.POST("/user", PostUserHandler)
 	err := r.Run(":8083")
 	if err != nil {
 		log.Fatalf("impossible to start server: %s", err)
